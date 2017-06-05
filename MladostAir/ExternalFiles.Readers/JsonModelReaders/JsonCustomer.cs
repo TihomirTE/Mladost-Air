4b5bbbc4bf8b90cbe3d30f8123eb6a093da728cf
TihomirTE <tihomirteofilov@gmail.com>
2017-06-05 21:47:05 +0300

    refactor json models

---
+++ MladostAir/ExternalFiles.Readers/JsonModelReaders/JsonCustomer.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExternalFiles.Readers.JsonModelReaders
+{
+    class JsonCustomer
+    {
+    }
+}
