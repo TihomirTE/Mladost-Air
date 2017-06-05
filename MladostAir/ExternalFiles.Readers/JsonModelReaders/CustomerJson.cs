8eb4b8850e417ac6d7e24b98e5b12fc3f5aecbf0
TihomirTE <tihomirteofilov@gmail.com>
2017-06-05 20:48:17 +0300

    Add json model readers

---
+++ MladostAir/ExternalFiles.Readers/JsonModelReaders/CustomerJson.cs
@@ -0,0 +1,19 @@
+﻿using Newtonsoft.Json;
+
+namespace ExternalFiles.Readers.JsonModelReaders
+{
+    public class CustomerJson
+    {
+        [JsonProperty("firstName")]
+        public string FirstName { get; set; }
+
+        [JsonProperty("lastName")]
+        public string LastName { get; set; }
+
+        [JsonProperty("customerNumber")]
+        public int CustomerNumber { get; set; }
+
+        [JsonProperty("age")]
+        public int Age { get; set; }
+    }
+}
---
+++ MladostAir/ExternalFiles.Readers/JsonModelReaders/DataArray.cs
@@ -0,0 +1,11 @@
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
+
+namespace ExternalFiles.Readers.JsonModelReaders
+{
+    public class DataArray
+    {
+        [JsonProperty("data")]
+        public List<TicketJson> Superheroes { get; set; }
+    }
+}
