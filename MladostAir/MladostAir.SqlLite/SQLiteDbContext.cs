579d92c532be54ec2fc9177a38dc958224c825f1
TihomirTE <tihomirteofilov@gmail.com>
2017-06-12 22:04:30 +0300

    Add Sqlite

---
+++ MladostAir/MladostAir.SqlLite/SQLiteDbContext.cs
@@ -0,0 +1,31 @@
+﻿using MladostAir.SqlLite.Contracts;
+using MladostAir.SqlLite.Models;
+using SQLite.CodeFirst;
+using System.Data.Entity;
+
+namespace MladostAir.SqlLite
+{
+    public class SQLiteDbContext : DbContext, ISqliteDatabase
+    {
+        public SQLiteDbContext()
+            : base("Hotels")
+        {
+            //Database.SetInitializer(new MigrateDatabaseToLatestVersion<SQLiteDbContext, Configuration>());
+        }
+
+        public virtual DbSet<Hotel> Hotels { get; set; }
+
+        public virtual DbSet<City> Cities { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            var sqliteConnectionInitializer = new SqliteCreateDatabaseIfNotExists<SQLiteDbContext>(modelBuilder);
+            Database.SetInitializer(sqliteConnectionInitializer);
+        }
+
+        public override int SaveChanges()
+        {
+            return base.SaveChanges();
+        }
+    }
+}
