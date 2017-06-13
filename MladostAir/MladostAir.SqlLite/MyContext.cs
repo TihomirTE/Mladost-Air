51242f17bcb6be14b3c63249ee3406b8cd723506
TihomirTE <tihomirteofilov@gmail.com>
2017-06-13 10:33:27 +0300

    Add Sqlite myContext

---
+++ MladostAir/MladostAir.SqlLite/MyContext.cs
@@ -0,0 +1,15 @@
+﻿using SQLite.CodeFirst;
+using System.Data.Entity;
+
+namespace MladostAir.SqlLite
+{
+    public class MyContext : DbContext
+    {
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            var model = modelBuilder.Build(Database.Connection);
+            IDatabaseCreator sqliteDatabaseCreator = new SqliteDatabaseCreator();
+            sqliteDatabaseCreator.Create(Database, model);
+        }
+    }
+}
